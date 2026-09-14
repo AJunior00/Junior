e61a316a5dda953238e2b057caee9f6abb6c949d
JuniorCode <chavesjr221@gmail.com>
2026-09-14 16:10:11 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
     <title>bla bla sla</title>
 </head>
 <body>
+
+    $nome = "Mr_junior";
     <?php
-    echo"<h1>meu nome é BRUTO, sobrenome é MEMO</h1>
-    <h2>Bruto Memo</h2>"?>
+    echo"<h1>Olá, $nome </h1>"?>
 </body>
 </html>
