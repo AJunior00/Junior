9fdcd8c89714a77e21260ccacfc4a54005696e47
JuniorCode <chavesjr221@gmail.com>
2026-09-14 15:27:45 -0300

    Update index.php
    .....

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-echo"<h1>Olá Tropa <h1>";
+<!DOCTYPE html>
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bla bla sla</title>
+</head>
+<body>
+    <?php
+    echo"<h1>Salve rapazes</h1>
+    <h2>salvezinho</h2>"?>
+</body>
+</html>
