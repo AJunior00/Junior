2ab371730d2109ba7c99a41954ffa5d4df290f9b
JuniorCode <chavesjr221@gmail.com>
2026-09-15 16:45:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
+<div class="card">
+<h1>nome: <?= $nome?> </h1>
+        <h2>idade: <?= $idade?></h2>
+        <p>mostrar: <?= $mostrar?></p>
 
+</div>
 
 <!DOCTYPE html>
 <html lang="PT-br">
@@ -11,32 +16,52 @@
 <body>
 
 
-<form action="">
+<form method="POST">
+
+    <input type="text" id="nome" name="nome">
 
     <input type="text" name="$nome" placeholder="Digite seu Nome">
 
     <input type="number" id="$idade" placeholder="Digite sua idade">
 
-    <input type="button" value="Enviar">
+    <input type="submite" value="Enviar">
     </form>
 
+    <div class="card">
 
-    <?php
-    $nome = "Mr_junior";
-    $idade = "17";
-    $mostar = "";
+        
+        <h1>Mostrando idade</h1>
+        
+        <?php
+
+$nome = "";
+$idade = 0;
+$mostar = "";
+if ($_SERVER ["REQUEST_METHOD"] == "POST"){
+    
+    $nome = $_POST["nome"];
+    $idade = $_POST["idade"];
+    
     if ($idade >=18){
         $mostrar = "Você é maior de idade";
     }
     else {
         $mostrar = "Você é menor de idade!";
     }
-    ?>
-    <div class="card">
-
-        <h1>nome: <?= $nome?> </h1>
-        <h2>idade: <?= $idade?></h2>
-        <p>mostrar: <?= $mostrar?></p>
+}
+?>
+    
+    <?php if ($mostrar != "") { ?>
+        
+        <h2>
+            o <?= $nome ?> é <?= $mostrar ?> de idade. 
+        </h2>
+        
+        <?php } ?>
+    
     </div>
-</body>
-</html>
+        
+    </body>
+    </html>
+    
+    
