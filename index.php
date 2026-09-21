a50684cb2ed5bc26cc214f875d0a1c48eea11260
analima48-cell <ana.l.lima48@aluno.senai.br>
2026-09-21 14:48:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ if ($_SERVER ["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
+    <a href="arquivo.php">Atividade x</a>
     <title>bla bla sla</title>
 </head>
 <body>
