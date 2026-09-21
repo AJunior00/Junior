f381f3cd0fa2d3b75a77d3058082fde1d8bc1992
analima48-cell <ana.l.lima48@aluno.senai.br>
2026-09-21 17:11:37 -0300

    .

--- notas.php
+++ notas.php
@@ -43,6 +43,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notas!</title>
+            <link rel="stylesheet" href="index.css">
+
 </head>
 <body>
         <h1>Cadastro do aluno</h1>
