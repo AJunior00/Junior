a0ea5b73dd855fa92902b0fe14bebfbdbd0a3704
JuniorCode <chavesjr221@gmail.com>
2026-09-22 16:43:30 -0300

    Update notas.php

--- notas.php
+++ notas.php
@@ -51,22 +51,38 @@
 
     <form method="POST">
         <input type="text" name="nome" placeholder="Digite seu nome" required>
+        
         <br><br>
+        
         <input type="number" name="idade" placeholder="Digite sua idade" required>
+        
         <br><br>
+        
         <input type="number" name="nota1" placeholder="Nota 1" step="0.1" required>
+        
         <br><br>
+        
         <input type="number" name="nota2" placeholder="Nota 2" step="0.1" required>
+        
         <br><br>
+        
         <input type="number" name="nota3" placeholder="Nota 3" step="0.1" required>
+        
         <br><br>
+        
         <input type="number" name="nota4" placeholder="Nota 4" step="0.1" required>
+        
         <br><br>
+        
         <input type="number" name="nota5" placeholder="Nota 5" step="0.1" required>
+        
         <br><br>
+        
         <button type="submit">Enviar</button>
     </form>
 
+    <br>
+    
     <?php if ($_SERVER["REQUEST_METHOD"] === "POST") { ?>
         <div class="card">
             <h1>Resultado</h1>
